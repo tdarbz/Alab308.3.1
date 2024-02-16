55e1e752f4f2db5968456211d55b41f6fd7a898b
tdarbz <darbytristan@gmail.com>
2024-02-15 21:16:26 -0500

    part 2 added Prime Time

--- 308.3.js
+++ 308.3.js
@@ -1,13 +1,17 @@
 //Fizz Buzz problem
+console.log("Fizz Buzz problem")
 
 let x = 1;
 while (x <= 100){
+    //if statement that checks if the number is divisble by 3 & 5 logs Fizz Buzz to the screen
     if (x % 3 == 0 && x % 5 == 0) {
         console.log("Fizz Buzz")
         x++;
+     // else if checks if divisible by 3 logs Fizz to the screen
     } else if (x % 3 == 0) {
         console.log("Fizz")
         x++;
+    // else if that checks if divisible by 5 logs Buzz to the screen
     } else if (x % 5 == 0) {
         console.log("Buzz")
         x++;
@@ -17,7 +21,28 @@ while (x <= 100){
     }
 }
 
+console.log('')
+console.log("Prime Time")
+
 //Prime Time
 
+let n = 7;
 
+// Function to check if a number is prime
+function isPrime(num) {
+  for(let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++)
+    if(num % i === 0) return false; 
+  return num > 1;
+}
+
+// Create a loop that searches for the next prime number, starting at n and incrementing from there.
+let nextPrime = n;
+do {
+  nextPrime++;
+} while (!isPrime(nextPrime));
+
+// As soon as you find the prime number, log that number and exit the loop.
+console.log(nextPrime);
+
+console.log('')
 
